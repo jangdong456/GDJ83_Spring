dc1f8f818c277c2e31c55e9ea7bf6efd67c38897
jangdongguk <jangdong456@gmail.com>
2024-07-10 12:30:33 +0900

    sql mapper if문 활용

--- src/main/java/com/jang/app/transfers/TransferService.java
+++ src/main/java/com/jang/app/transfers/TransferService.java
@@ -3,17 +3,34 @@ package com.jang.app.transfers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jang.app.accounts.AccountDAO;
+import com.jang.app.accounts.AccountDTO;
+
 @Service
 public class TransferService {
 	
 	@Autowired
 	private TransferDAO transferDAO;
 	
+	@Autowired
+	private AccountDAO accountDAO;
+	
 	public int trade(TransferDTO transferDTO)throws Exception {
 		// 1번 계좌에서 5000을 1720369109171 계좌로 이체
 		// 보내는 계좌
 		
-//		transferDTO.setProduct_info_id(1);
+		
+		// ===============
+		// 잔액이 보내는 값보다 작을 때
+		AccountDTO accountDTO = new AccountDTO();
+		accountDTO.setProduct_info_id(transferDTO.getProduct_info_id());
+	
+		accountDTO = accountDAO.detail(accountDTO);
+		if(accountDTO.getBalance() < transferDTO.getBalance()) {
+			return 0;
+		}
+		
+		transferDTO.setProduct_info_id(1);
 		transferDTO.setAmount(transferDTO.getAmount()*-1);
 		transferDTO.setType("출금");
 		
